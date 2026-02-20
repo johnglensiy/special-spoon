479ff46bd8a7030e506d70c5b269591770b26f70
John Glen Siy <johnglen_siy@berkeley.edu>
2026-02-20 13:28:12 -0800

    feat: init backend server for running scraper scripts

---
+++ src/backend/server.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from scraper import scrape_data 
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/results")
+def get_results():
+    data = scrape_data()
+    print(data)
+    return data
